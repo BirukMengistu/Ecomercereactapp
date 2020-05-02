75833d76353de3ce62f57d6f731a302b58600a6f
BirukMengistu <sheger.shelf3@gmail.com>
2020-05-02 20:49:56 +0200

    modifiy layout

--- src/homepage.comp.jsx
+++ src/homepage.comp.jsx
@@ -3,7 +3,7 @@ import './homepage.styles.scss';
 export const HomePage = () => {
     return (
         <div className = 'homepage'>
-        <div className = 'Dirctory-menu'>
+        <div className = 'directory-menu'>
            <div className = 'menu-item'>
             <div className = 'content'>
                 <h1 className = 'title'> Hats</h1>
